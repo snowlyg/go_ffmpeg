020eb4733b92a133a5e6ff419a957534891da779
snowlyg <569616226@qq.com>
2020-06-12 17:36:08 +0800

    增加 ffmpeg 参数设置，增加 dll 推送

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 /*
 #cgo CFLAGS: -DPNG_DEBUG=1 -I./ffmpeg/include -I./hls
-#cgo LDFLAGS: -L${SRCDIR}/ffmpeg/lib -llibavformat -llibavcodec -llibavutil -llibavdevice -llibavfilter -llibswresample -llibswscale
+#cgo LDFLAGS: -L./ffmpeg/lib -llibavformat -llibavcodec -llibavutil -llibavdevice -llibavfilter -llibswresample -llibswscale
 #include <hls.c>
 */
 import "C"
