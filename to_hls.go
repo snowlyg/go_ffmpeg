0fe7936d166b28dee846de2263848a3b85e47f41
snowlyg <569616226@qq.com>
2020-06-18 07:44:55 +0800

    增加传参，修改参数为外部传入

--- to_hls.go
+++ to_hls.go
@@ -150,8 +150,9 @@ if (output_format_context && !(output_format_context->oformat->flags & AVFMT_NOF
 */
 import "C"
 
-func main() {
-	inFilename := "rtsp://183.59.168.27/PLTV/88888905/224/3221227272/10000100000000060000000001030757_0.smil?icip=88888888"
-	outFilename := "D:/Env/nginx/html/hls/ffmpeg/test.m3u8"
+//	inFilename := "rtsp://183.59.168.27/PLTV/88888905/224/3221227272/10000100000000060000000001030757_0.smil?icip=88888888"
+//	outFilename := "D:/Env/nginx/html/hls/ffmpeg/test.m3u8"
+func ToHls(inFilename, outFilename string) {
+	outFilename = outFilename + "/out.m3u8"
 	C.to_hls(C.CString(inFilename), C.CString(outFilename))
 }
