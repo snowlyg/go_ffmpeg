874c6376a6d0df7592e8347ee707bedd1856b8d0
snowlyg <569616226@qq.com>
2021-04-13 08:32:43 +0800

    修改 example

--- example/hls_server/main.go
+++ example/hls_server/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/snowlyg/go_ffmpeg/example/hls_server/routers"
 	"github.com/snowlyg/go_ffmpeg/src"
 )
 
 func main() {
+	// 初始化路由
+	err := routers.Init()
+	if err != nil {
+		return
+	}
+
 	go hls(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+	go startHTTP()
+
 	select {}
 }
 
@@ -31,4 +43,20 @@ func hls(in, out, ht, hl string) {
 	if err != nil {
 		println(err)
 	}
+}
+
+func startHTTP() {
+	httpServer := http.Server{
+		Addr:              ":10008",
+		Handler:           routers.Router,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+	link := "http://localhost:10008"
+	log.Println("http server start -->", link)
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Println("start http server error", err)
+		}
+		log.Println("http server start")
+	}()
 }
--- example/simple/main.go
+++ example/simple/main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/snowlyg/go_ffmpeg/example/hls_server/routers"
 	"github.com/snowlyg/go_ffmpeg/src"
 )
 
 func main() {
-	// 初始化路由
-	err := routers.Init()
-	if err != nil {
-		return
-	}
-
 	go hls(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-	go startHTTP()
-
 	select {}
 }
 
@@ -43,20 +31,4 @@ func hls(in, out, ht, hl string) {
 	if err != nil {
 		println(err)
 	}
-}
-
-func startHTTP() {
-	httpServer := http.Server{
-		Addr:              ":10008",
-		Handler:           routers.Router,
-		ReadHeaderTimeout: 5 * time.Second,
-	}
-	link := "http://localhost:10008"
-	log.Println("http server start -->", link)
-	go func() {
-		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Println("start http server error", err)
-		}
-		log.Println("http server start")
-	}()
 }
